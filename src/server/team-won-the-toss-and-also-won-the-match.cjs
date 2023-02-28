47fa91b3735a5faafff6110955c1e5225d6631c6
Madan Gopal <madan.23.2@mountblue.tech>
2023-02-28 16:03:01 +0530

    Initial commit Added team-won-the-toss-and-also-won-the-match file

---
+++ src/server/team-won-the-toss-and-also-won-the-match.cjs
@@ -0,0 +1,42 @@
+const fs = require('fs');
+const { getgroups } = require('process');
+const csvToObj = require('csv-to-js-parser').csvToObj;
+const data = fs.readFileSync('../data/matches.csv').toString();
+
+const matchDescription = {
+    id: { type: 'number', group: 1 },
+    toss_winner: { type: 'string' },
+    winner: { type: 'string' }
+};
+
+const iplMatchData = csvToObj(data, ',', matchDescription);
+
+function teamWonTheTossAndTheMatch() {
+    const tossAndMatchWinner = {};
+
+    for (let index = 0; index < iplMatchData.length; index++) {
+        let matchWinner = iplMatchData[index].winner[0];
+        let tossWinner = iplMatchData[index].toss_winner[0];
+
+        if (matchWinner === tossWinner) {
+            if (tossAndMatchWinner[matchWinner] = tossAndMatchWinner[matchWinner]) {
+                tossAndMatchWinner[matchWinner] += 1;
+            } else {
+                tossAndMatchWinner[matchWinner] = 1;
+            }
+        }
+    }
+    const jsonObj = (JSON.stringify(tossAndMatchWinner));
+
+    fs.writeFile("../public/output/team-won-the-toss-and-also-won-the-match.json", jsonObj, 'utf8', function (err) {
+        if (err) {
+            console.log("An error occured while writing JSON Object to File.");
+            return console.log(err);
+        }
+
+        console.log("JSON file has been saved.");
+    });
+
+}
+teamWonTheTossAndTheMatch();
+module.exports = teamWonTheTossAndTheMatch();
