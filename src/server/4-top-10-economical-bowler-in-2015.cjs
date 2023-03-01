d5b558a7da30ced86adbd0d9fab5c02d7f441120
Madan Gopal <madan.23.2@mountblue.tech>
2023-03-02 00:51:31 +0530

    Added loop to get the best economy player in object

--- src/server/4-top-10-economical-bowler-in-2015.cjs
+++ src/server/4-top-10-economical-bowler-in-2015.cjs
@@ -23,7 +23,7 @@ const deliveryDescription = {
     total_runs: { type: 'number' },
     bye_runs: { type: 'number' },
     legbye_runs: { type: 'number' }
-    
+
 };
 const deliveryData = csvToObj(deliveriesData, ',', deliveryDescription);
 const iplMatchData = csvToObj(data, ',', matchDescription);
@@ -37,44 +37,62 @@ for (const key in iplMatchData) {
     }
 }
 // console.log(matchId);
+function topBowlerEconomyIn() {
 
-const bowlerEconomy = {};
-for (const key in deliveryData) {
-    if (matchId[deliveryData[key].match_id]) {
+    const bowlerEconomy = {};
+    for (const key in deliveryData) {
+        if (matchId[deliveryData[key].match_id]) {
 
-        for (let index = 0; index < deliveryData[key].bowler.length; index++) {
+            for (let index = 0; index < deliveryData[key].bowler.length; index++) {
 
-            let run = 0;
-            if (deliveryData[key].total_runs[index]) {
-                run = deliveryData[key].total_runs[index];
-            }
-            if (deliveryData[key].wide_runs[index]) {
-                run = run - deliveryData[key].total_runs[index];
-            }
-            if (deliveryData[key].noball_runs[index]) {
-                run = run - deliveryData[key].noball_runs[index];
-            }
-            if (deliveryData[key].bye_runs[index]) {
-                run = run - deliveryData[key].bye_runs[index];
-            }
-            if (deliveryData[key].legbye_runs[index]) {
-                run = run - deliveryData[key].legbye_runs[index];
-            }
+                let run = 0;
+                if (deliveryData[key].total_runs[index]) {
+                    run = deliveryData[key].total_runs[index];
+                }
+                if (deliveryData[key].wide_runs[index]) {
+                    run = run - deliveryData[key].total_runs[index];
+                }
+                if (deliveryData[key].noball_runs[index]) {
+                    run = run - deliveryData[key].noball_runs[index];
+                }
+                if (deliveryData[key].bye_runs[index]) {
+                    run = run - deliveryData[key].bye_runs[index];
+                }
+                if (deliveryData[key].legbye_runs[index]) {
+                    run = run - deliveryData[key].legbye_runs[index];
+                }
 
-            if (!bowlerEconomy[deliveryData[key].bowler[index]]) {
-                bowlerEconomy[deliveryData[key].bowler[index]] = [run, 1];
-            } else {
-                bowlerEconomy[deliveryData[key].bowler[index]] = [bowlerEconomy[deliveryData[key].bowler[index]][0] + run, bowlerEconomy[deliveryData[key].bowler[index]][1] + 1]
+                if (!bowlerEconomy[deliveryData[key].bowler[index]]) {
+                    bowlerEconomy[deliveryData[key].bowler[index]] = [run, 1];
+                } else {
+                    bowlerEconomy[deliveryData[key].bowler[index]] = [bowlerEconomy[deliveryData[key].bowler[index]][0] + run, bowlerEconomy[deliveryData[key].bowler[index]][1] + 1]
+                }
+
             }
 
         }
-
     }
-}
-for (const key in bowlerEconomy) {
-    let overs = (bowlerEconomy[key][1] / 6);
-    bowlerEconomy[key] = (bowlerEconomy[key][0] / overs);
+    for (const key in bowlerEconomy) {
+        let overs = (bowlerEconomy[key][1] / 6);
+        bowlerEconomy[key] = (bowlerEconomy[key][0] / overs);
+        let economy = bowlerEconomy[key];
+        bowlerEconomy[key] = parseFloat(economy.toFixed(2));
+    }
 
-}
+    const topBowlerEconomy = {};
+    for (let index = 0; index < 10; index++) {
 
-console.log(bowlerEconomy);
+        let smallestValue = Number.MAX_VALUE;
+        let selectedKey = '';
+        for (let key in bowlerEconomy) {
+            if (bowlerEconomy[key] < smallestValue) {
+                smallestValue = bowlerEconomy[key];
+                selectedKey = key;
+            }
+        }
+        topBowlerEconomy[selectedKey] = smallestValue;
+        delete bowlerEconomy[selectedKey];
+    }
+    console.log(topBowlerEconomy);
+}
+topBowlerEconomyIn();
